f756cb390b52779c515d23366b10f31d36915309
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-18 04:02:56 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,8 @@
-## code your solution here. 
+class Cat
+
+  attr_accessor :name  #getter/reader and setter/writer methods combined 
+
+  def meow
+    puts "meow!"
+  end
+end ## code your solution here. 
